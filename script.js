63242254142fe36bf7d79a245cc988259a32c492
cmfernandes <cristianafmfernandes@gmail.com>
2023-04-26 15:07:04 +0100

    add rainbow btn

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 let gridContainer = document.getElementById("grid-container");
-let gridBtn = document.getElementById("gridBtn");
+let inputBtn = document.getElementById("inputBtn");
+let rainbowBtn = document.getElementById("rainbowBtn")
 let numRow = 16;
 let numCol = 16;
 let cell
 const resetBtn = document.getElementById("resetBtn")
 
 function getUserInput(){
-    let userInputWidth = prompt("Enter the number of cells for the with. (Min 1 and Max 100")
+    let userInputWidth = prompt("Enter the number of cells for the width. (Min 1 and Max 100")
     while(userInputWidth  < 1 || userInputWidth  > 100 || !parseInt(userInputWidth)){
-        userInputWidth = prompt("Enter a number between 1 and 100 for the with.")
+        userInputWidth = prompt("Enter a number between 1 and 100 for the width.")
         
     }
     let userInputHeight = prompt("Enter the number of cells for the height. (Min 1 and Max 100")
@@ -25,6 +26,9 @@ function getUserInput(){
 
 function makeGrid(numCol,numRow){
     gridContainer.textContent = "";
+    let cellWidth = gridContainer.clientWidth / numCol;
+    let cellHeight = gridContainer.clientHeight / numRow;
+
     for(let i = 0; i < numCol; i++){
         let col = document.createElement("div");
         col.id = "col";
@@ -33,6 +37,9 @@ function makeGrid(numCol,numRow){
             cell = document.createElement("div");
             cell.className = "cell";
             
+            cell.style.width = `${cellWidth}px`;
+            cell.style.height = `${cellHeight}px`;
+
             col.appendChild(cell);
         }
         
@@ -57,12 +64,26 @@ function reset(){
 }
 
 
-gridBtn.addEventListener('click', () => {
+inputBtn.addEventListener('click', () => {
     getUserInput()
 })
 
-resetBtn.addEventListener('click', ()=> {
+resetBtn.addEventListener('click', () => {
     reset()})
+
+rainbowBtn.addEventListener('click', () => {
+    document.querySelectorAll(".cell").forEach(item => {
+        item.addEventListener('mouseover', () => {
+            item.style.backgroundColor = randomColor();
+        })
+    })
+})
+function randomColor(){
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
 
 
 function setup() {
