2aa4a21a0a4436851e88d2dcb873824b7d9cde1d
cmfernandes <cristianafmfernandes@gmail.com>
2024-02-09 14:33:18 +0000

    Refactor event listeners and button functionalities

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 let gridContainer = document.getElementById("grid-container");
 let inputBtn = document.getElementById("inputBtn");
-let rainbowBtn = document.getElementById("rainbowBtn")
+
+
 let numRow = 16;
 let numCol = 16;
 let cell
-const resetBtn = document.getElementById("resetBtn")
+
 
 function getUserInput(){
-    let userInputWidth = prompt("Enter the number of cells for the width. (Min 1 and Max 100")
+    let userInputWidth = prompt("Enter the number of cells for the width. (Min 1 and Max 100)")
     while(userInputWidth  < 1 || userInputWidth  > 100 || !parseInt(userInputWidth)){
         userInputWidth = prompt("Enter a number between 1 and 100 for the width.")
         
     }
-    let userInputHeight = prompt("Enter the number of cells for the height. (Min 1 and Max 100")
+    let userInputHeight = prompt("Enter the number of cells for the height. (Min 1 and Max 100)")
     while(userInputHeight < 1 || userInputHeight > 100 || !parseInt(userInputHeight)){
         userInputHeight = prompt("Enter a number between 1 and 100 for the height.")
         
@@ -20,8 +21,7 @@ function getUserInput(){
     numCol = userInputWidth;
     numRow = userInputHeight;
 
-    setup()
-   
+    setup()  
 }
 
 function makeGrid(numCol,numRow){
@@ -41,54 +41,65 @@ function makeGrid(numCol,numRow){
             cell.style.height = `${cellHeight}px`;
 
             col.appendChild(cell);
-        }
-        
+        };
         gridContainer.appendChild(col);
+    };
+};
 
-    }
-
-}
-
-function paint(){   
+function black(){   
     document.querySelectorAll(".cell").forEach(item => {
         item.addEventListener('mouseover', () => {
             item.style.backgroundColor = "black"
-        })
-    })
-} 
+        });
+    });
+} ;
+
 
 function reset(){
     numCol = 16;
     numRow = 16;
     setup();
-}
+};
 
 
 inputBtn.addEventListener('click', () => {
     getUserInput()
-})
+});
 
+const resetBtn = document.getElementById("resetBtn");
 resetBtn.addEventListener('click', () => {
-    reset()})
+    reset()
+});
 
+
+let blackBtn = document.getElementById("blackBtn");
+blackBtn.addEventListener('click', () => {
+    black();
+});
+
+
+let rainbowBtn = document.getElementById("rainbowBtn");
 rainbowBtn.addEventListener('click', () => {
     document.querySelectorAll(".cell").forEach(item => {
         item.addEventListener('mouseover', () => {
             item.style.backgroundColor = randomColor();
-        })
-    })
-})
+        });
+    });
+});
+
+
+
 function randomColor(){
     var r = Math.floor(Math.random() * 256);
     var g = Math.floor(Math.random() * 256);
     var b = Math.floor(Math.random() * 256);
     return "rgb(" + r + ", " + g + ", " + b + ")";
-}
+};
 
 
 function setup() {
     makeGrid(numCol,numRow);
-    paint();
-}
+    black();
+};
 
 setup();
